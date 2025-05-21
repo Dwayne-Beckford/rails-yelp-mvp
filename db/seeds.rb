82fda205e803479955fc270a2c65365aec03849c
dwayne beckford <beckfordd33@hotmail.com>
2025-05-21 16:55:57 +0100

    challenge 2 stuck on user stories journey

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+five_guys = Restaurant.create!(name: "Five Guys", address:"East London", phone_number:"12345678903", category:"belgian")
+dixy = Restaurant.create!(name: "Dixy", address:"North London", phone_number:"09876543212", category:"french")
+mickey = Restaurant.create!(name: "Mickey", address:"South London", phone_number:"09898987876", category:"japanese")
+pizza_hut = Restaurant.create!(name: "Pizza Hut", address:"West London", phone_number:"76876543434", category:"italian")
+happy_valley = Restaurant.create!(name: "Happy Valley", address:"North London", phone_number:"98767898789", category:"chinese")
+
+puts "Finished!"
